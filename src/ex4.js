e16630a1f5b214dd6524192e7aa9eeb201bf6912
Robin B <robin.buergi@gmail.com>
2017-12-07 13:49:55 +0100

    added delay

--- src/ex4.js
+++ src/ex4.js
@@ -2,16 +2,25 @@
 4. Streams & Lambdas
 Implementieren Sie eine Methode public String processNames(String[] names), welche für einen Array von Strings alle Strings mit einer Länge 
 zwischen 3 und 4 (je inklusiv) in Grossbuchstaben umwandelt und, mit einem Abstand dazwischen, zusammen hängt.
-Für den Array {"Susanna", "Joe", "Lu", "Timmy", "Rafael", "Lisa"} soll die Rückgabe also wie folgt aussehen: "JOE LISA".
+Für den Array {"Susanna", "Joe", "Lu", "Timmy", "Rafael", "Lisa"} soll die Rückgabe also wie folgt aussehen: "JOE LISA ".
 Hinweise: Verwenden Sie dazu einen String-Stream sowie passende Aggregate-Operationen.
 Verwenden Sie an passender Stelle eine Methoden-Referenz auf eine String-Methode.
 */
 
 var Rx = require('rxjs/Rx');
 
-data = Rx.Observable.of("Susanna", "Joe", "Lu", "Timmy", "Rafael", "Lisa");
+var names = ["Susanna", "Joe", "Lu", "Timmy", "Rafael", "Lisa", "Tim", "Tom", "Test"];
 
-data.filter(x => x.length >= 3 && x.length <= 4)
-    .map(x => x.toUpperCase())
-    .reduce((x, y) => x + " " + y)
-    .subscribe(x => console.log(x))
+var source = Rx.Observable.create(function (observer) {
+    var interval = setInterval(() => {
+        observer.next(names.pop());
+        if (names.length <= 0) {
+            observer.complete();
+            clearInterval(interval);
+        }
+    }, 1000);
+});
+
+source.filter(x => x.length >= 3 && x.length <= 4)
+    .map(x => x.toUpperCase() + " ")
+    .subscribe(x => process.stdout.write(x));
