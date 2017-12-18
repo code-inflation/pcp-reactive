21f711ad9c4081910e33b95301b50afe1c8acdee
Dorus Janssens <dorus.janssens@gmail.com>
2017-12-18 17:12:18 +0100

    Update component

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -15,27 +15,50 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   transactions$: Rx.Observable<any>;
+  transactionSubject$ = new Rx.Subject<any>();
   transactionEvents = [];
+
+  sumBitcoin = 0;
+  countTransactions = 0;
 
   ngOnInit() {
 
+    // Create websocket
     const ws = new WebSocket('wss://ws.blockchain.info/inv');
 
+    // Start stream
     ws.addEventListener('open', function open() {
       ws.send('{"op":"unconfirmed_sub"}');
     });
 
-    this.transactions$ = Rx.Observable.fromEvent(ws, 'message');
-
-    this.transactions$
+    // Receive event and map data to object
+    this.transactions$ = Rx.Observable
+      .fromEvent(ws, 'message')
       .map((event: MessageEvent) => JSON.parse(event.data))
-      .do(console.log)
-      .filter(data => data.x.inputs[0].prev_out.value/100000000 > 5)
+
+    // Emit transactions to subject
+    this.transactions$.subscribe(data => {
+      this.transactionSubject$.next(data);
+    })
+
+    // Get data for displaying list of 10 latest transactions higher than 5 BTC
+    this.transactionSubject$
+      .filter(data => data.x.inputs[0].prev_out.value / 100000000 > 5)
       .subscribe(data => {
         this.transactionEvents.unshift(data);
         if (this.transactionEvents.length > 10) {
           this.transactionEvents.pop();
         }
       });
+
+    // Sum total transaction amount
+    this.transactionSubject$
+      .scan((acc, data) => Math.round(acc + data.x.inputs[0].prev_out.value / 100000000), 0)
+      .subscribe(sum => this.sumBitcoin = sum);
+
+    // Sum total transaction amount
+    this.transactionSubject$
+      .scan((acc, data) => acc + 1, 0)
+      .subscribe(count => this.countTransactions = count);
   }
 }
